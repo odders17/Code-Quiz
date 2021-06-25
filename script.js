99a3d5090cd9e7cf750dd48bdd79a0a2983119f0
Paul Aston <odders17@gmail.com>
2021-06-25 16:59:52 +0100

    amended errors in script to fix quiz

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var nextButton = document.getElementById('next-btn')
 var questionContainerElement = document.getElementById
 ('question-container')
 var questionElement = document.getElementById('question')
-var anwserButtonsElement = document.getElementById ('answer-buttons')
+var answerButtonsElement = document.getElementById ('answer-buttons')
 
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestion, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () =>{
@@ -16,7 +16,7 @@ nextButton.addEventListener('click', () =>{
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    shuffledQuestion = question.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
@@ -24,13 +24,13 @@ function startGame() {
 
 function setNextQuestion() {
     resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    showQuestion(shuffledQuestion[currentQuestionIndex])
 
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answer.forEach(answer => {
+    question.answers.forEach(answer => {
         var button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
@@ -38,27 +38,27 @@ function showQuestion(question) {
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
-        anwserButtonsElement.appendChild(button)
+        answerButtonsElement.appendChild(button)
     })
 }
 
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
-    while (anwserButtonsElement.firstChild) {
-        anwserButtonsElement.removeChild
-        (anwserButtonsElement.firstChild)
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
     }
 }
 
-function selectAnwser(e) {
+function selectAnswer(e) {
     var selectedButton = e.target
     var correct = selectedButton.dataset.correct
     setStatusClass(document.body,correct)
-    Array.from(answerButtonElement.children).forEach(button => {
+    Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (shuffledQuestions.lenght > currentQuestionIndex + 1) {
+    if (shuffledQuestion.lenght > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
     } else {
         startButton.innerText = 'Restart'
@@ -80,57 +80,59 @@ function clearStatusClass(element) {
     element.classList.remove('wrong')
 }
 
-var questions = [
+var question = [
     {
-        question: 'What is 2+2?',
+        question: 'Do you have to be good at math to be a developer?',
         answers: [
-            { text: '4', correct: true},
-            { text:'22', correct: false}
+            { text: 'No', correct: true},
+            { text:'Yes', correct: false},
+            { text: 'Maybe', correct: false},
+            { text: 'Think so', correct: false},
         ]
     },
     {
-        question: 'Who is the best footballer?',
+        question: 'What is HTML?',
         answers: [
-        { text: 'Jack Grealish', correct: true},
-        { text: 'Mpbappe', correct: false},
-        { text: 'Messi', correct: false},
-        { text: 'Ronaldo', correct: false},
+        { text: 'HydroTomoto Makeup', correct: false},
+        { text: 'HypeTool Mashup', correct: false},
+        { text: 'HenryTank Melon', correct: false},
+        { text: 'HyperText Markup', correct: true},
         ]
     },
     {
-        question: 'Who is the best footballer?',
+        question: 'What is CSS?',
         answers: [
-        { text: 'Jack Grealish', correct: true},
-        { text: 'Mpbappe', correct: false},
-        { text: 'Messi', correct: false},
-        { text: 'Ronaldo', correct: false},
+        { text: 'Control Semantic Styles', correct: false},
+        { text: 'Cascading Style Sheets', correct: true},
+        { text: 'Computer Source Sheet', correct: false},
+        { text: 'Colouful Sound Style', correct: false},
         ]
     },
     {
-        question: 'Who is the best footballer?',
+        question: 'What is Java Script?',
         answers: [
-        { text: 'Jack Grealish', correct: true},
-        { text: 'Mpbappe', correct: false},
-        { text: 'Messi', correct: false},
-        { text: 'Ronaldo', correct: false},
+        { text: 'An object based scripting language', correct: true},
+        { text: 'Is embedded Inside web Browsers', correct: true},
+        { text: 'Interprets script contained in web browsers', correct: true},
+        { text: 'It cannot read or write files', correct: true},
         ]
     },
     {
-        question: 'Who is the best footballer?',
+        question: 'What is a string in java script?',
         answers: [
-        { text: 'Jack Grealish', correct: true},
-        { text: 'Mpbappe', correct: false},
-        { text: 'Messi', correct: false},
-        { text: 'Ronaldo', correct: false},
+        { text: 'Stores a series of characters', correct: true},
+        { text: 'ties your shoes up', correct: false},
+        { text: 'Plays conkers with it', correct: false},
+        { text: 'holds up your trousers', correct: false},
         ]
     },
     {
-        question: 'Who is the best footballer?',
+        question: 'What is a push button?',
         answers: [
-        { text: 'Jack Grealish', correct: true},
-        { text: 'Mpbappe', correct: false},
-        { text: 'Messi', correct: false},
-        { text: 'Ronaldo', correct: false},
+        { text: 'chocolate sweets ', correct: false},
+        { text: 'fastens your shirts', correct: false},
+        { text: 'when user pushes a button a click event occurs', correct: true},
+        { text: 'Can be found on clothing', correct: false},
         ]
     },
 ]
